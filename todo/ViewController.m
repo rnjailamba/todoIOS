f948bda1ef9d448f2c173652c9acd2580076d3a7
rnjai <rnjailamba12@gmail.com>
2016-08-20 12:19:47 +0530

    cell added and shown

--- todo/ViewController.m
+++ todo/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+- (IBAction)addClicked:(id)sender;
+@property (nonatomic) NSMutableArray *todos;
 
 @end
 
@@ -16,7 +20,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.frame = [[UIScreen mainScreen]bounds];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    NSMutableArray *todos = [NSMutableArray arrayWithArray:@[@"First",@"Second"]];
+    _todos = todos;
+    [self registerNib];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)registerNib{
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"random"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +38,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)addClicked:(id)sender {
+
+}
+
+#pragma UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;    //count of section
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.todos count];    //count number of row from counting array hear cataGorry is An Array
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"random"];
+    cell.backgroundColor = [UIColor blackColor];
+    cell.frame = CGRectMake(0, 62 + indexPath.row*100, self.view.frame.size.width, 100);
+    return cell;
+}
 @end
